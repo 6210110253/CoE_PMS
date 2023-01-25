cc9476642742511951f8491b08aca337ca82bb02
6210110566 <6210110566@psu.ac.th>
2023-01-25 15:45:39 +0700

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,47 +1,15 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
-
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-<<<<<<< HEAD
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './node_modules/flowbite/**/*.js'
-=======
-        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
-        "./storage/framework/views/*.php",
-        "./resources/views/**/*.blade.php",
-        "./node_modules/flowbite/**/*.js",
->>>>>>> 5d76457bcdf236309bb5383d47a29e1b01b42406
-    ],
-
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ["Nunito", ...defaultTheme.fontFamily.sans],
-            },
-        },
-    },
-
-    variants: {
-        extend: {
-            opacity: ["disabled"],
-        },
-    },
-    experimental: {
-        applyComplexClasses: true,
-    },
-
-<<<<<<< HEAD
-
-
-    plugins: [
-        require('@tailwindcss/forms'), require('flowbite/plugin')
-    ],
-    
-        
-    
-=======
-    plugins: [require("@tailwindcss/forms"), require("flowbite/plugin")],
->>>>>>> 5d76457bcdf236309bb5383d47a29e1b01b42406
-};
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+    "./node_modules/flowbite/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
