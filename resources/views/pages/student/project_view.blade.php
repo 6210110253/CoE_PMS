d84b0b0ed68f923099a8657dc3b3c8e01e129e0d
ฟาตอนียะห์ หะยีมายอ <najwa.26252@gmail.com>
2023-02-07 19:58:26 +0700

    update ui project list

--- resources/views/pages/student/project_view.blade.php
+++ resources/views/pages/student/project_view.blade.php
@@ -3,22 +3,51 @@
         <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
             <div class="container mx-auto px-6 py-8">
 
-<div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    Topic
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    teacher
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    detail
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Status
+                </th>
 
-    <div class="px-5 pb-5">
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="bg-white border-b dark:bg-gray-900 dark:border-gray-700">
+                <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    **********
+                </th>
+                <td class="px-6 py-4">
+                    ********
+                </td>
+                <td class="px-6 py-4">
+                    <a href="{{ route('student.project.detail')}}" class="font-medium text-blue-600 dark:text-blue-500 hover:underline">view more</a>
+                </td>
+                <td class="px-6 py-4">
+                   ***********
+                </td>
 
-        <a >
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Topic</h5>
-            <p class="font-normal text-gray-700 dark:text-gray-400">Description : Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-        </a>
+
+            </tr>
 
 
-        <div class="flex items-center justify-between">
-            <span class="text-3xl font-bold text-gray-900 dark:text-white"></span>
-            <a href="" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">View Detail</a>
-        </div>
-    </div>
+
+
+        </tbody>
+    </table>
 </div>
+
+
 
 
 
