418a6bda7c6178b0548b1ba191a312ef3b2fe324
6210110566 <6210110566@psu.ac.th>
2023-02-07 14:34:46 +0700

    project detail (read more )

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -199,5 +199,60 @@
             </div>
         </main>
     </div>
+  
+    <div class="p-5 bg-amber-200">
+        <h1 class="mb-3 text-4xl font-light">ชื่อโปรเจกต์</h1>
+
+        <!-- This text always show up -->
+        <p>ดีเทล
+            <!-- The three dots -->
+            <span id="dots">...</span>
+
+            <!-- This content only be shown as needed -->
+            <span id="more" class="hidden">erisque enim ligula
+                venenatis dolor. Maecenas nisl est, ultrices nec congue eget, auctor vitae massa. Fusce luctus
+                vestibulum augue ut aliquet. Nunc sagittis dictum nisi, sed ullamcorper ipsum dignissim ac. In libero
+                sed nunc venenatis imperdiet sed ornare turpis. Donec vitae dui eget tellus gravida venenatis. Integer
+                fringilla congue eros non fermentum. Sed dapibus pulvinar nibh tempor porta.
+                <span>
+        </p>
+
+        <!-- Implement the read more/read less button -->
+        <button onclick="toggleText()" id="button"
+            class="mt-3 px-5 py-2 bg-rose-500 text-white duration-300 hover:bg-rose-700">Read
+            more</button>
+    </div>
+
+    <!-- Javascript code -->
+    <script>
+        function toggleText() {
+            var dots = document.getElementById("dots");
+            var moreText = document.getElementById("more");
+            var button = document.getElementById("button");
+
+            if (dots.classList.contains("hidden")) {
+                // Show the dots
+                dots.classList.remove("hidden");
+
+                // Hide the more text
+                moreText.classList.add("hidden");
+
+                // change text of the button
+                button.innerHTML = "Read more";
+            } else {
+                // Hide the dots
+                dots.classList.add("hidden");
+
+                // hide the more text
+                moreText.classList.remove("hidden");
+
+                // change text of the button
+                button.innerHTML = "Read less";
+            }
+        }
+    </script>
+
 </div>
 </x-app-layout>
+
+  
--- resources/views/pages/teacher/project_teacher.blade.php
+++ resources/views/pages/teacher/project_teacher.blade.php
@@ -41,101 +41,6 @@
 
                             <a href="{{ route('teacher.project.create')}}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create Project</a>
                         </div>
-                        <!-- Main modal -->
-                        {{-- <div id="defaultModal" tabindex="-1" aria-hidden="true" class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
-                            <div class="relative w-full h-full max-w-2xl md:h-auto">
-                                <!-- Modal content -->
-                                <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                                    <!-- Modal header -->
-                                    <div class="flex items-start justify-between p-4 border-b rounded-t dark:border-gray-600">
-                                        <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                                            Create Project
-                                        </h3>
-                                        <button type="button" class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white" data-modal-hide="defaultModal">
-                                            <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                                            <span class="sr-only">Close modal</span>
-                                        </button>
-                                    </div>
-                                    <!-- Modal body -->
-                                    <div class="p-6 space-y-6">
-                                        <form>
-                                            <div class="mb-6">
-                                                <label for="topic" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Topic</label>
-                                                <input type="text" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="Topic" required>
-                                            </div>
-                                            <div class="mb-6">
-
-                                                <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
-                                                <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
-
-                                            </div>
-                                            {{-- <div class="mb-6">
-
-                                                <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Scope</label>
-                                                <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
-
-                                            </div>
-                                            <div class="mb-6">
-
-                                                <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Objective</label>
-                                                <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
-
-                                            </div> --}}
-
-                                            {{-- <div class="mb-6">
-
-                                                <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Results</label>
-                                                <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
-
-                                            </div>
-
-                                            <div class="mb-6">
-
-                                                <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Condition</label>
-                                                <textarea id="message" rows="4" class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Leave a comment..."></textarea>
-
-                                            </div>
-
-                                            <div class="mb-6">
-                                            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="user_avatar">ER Diagram</label>
-                                            <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="user_avatar_help" id="user_avatar" type="file">
-                                            </div>
-
-                                            <div class="mb-6">
-                                            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="user_avatar">Design File</label>
-                                            <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="user_avatar_help" id="user_avatar" type="file">
-                                            </div>
-
-                                            <div class="mb-6">
-                                            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="user_avatar">Use Case</label>
-                                            <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="user_avatar_help" id="user_avatar" type="file">
-                                            </div>
-
-                                            <div class="mb-6">
-                                            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="user_avatar">Other</label>
-                                            <input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" aria-describedby="user_avatar_help" id="user_avatar" type="file">
-                                            </div>
-
-                                            <div class="mb-6">
-                                                <label for="topic" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Member</label>
-                                                <input type="text" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="Member" required>
-                                            </div> --}}
-
-                                            {{-- </form>
-                                    </div>
-                                    <!-- Modal footer -->
-                                    <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b dark:border-gray-600">
-                                        <button data-modal-hide="defaultModal" type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">ยืนยัน</button>
-                                        <button data-modal-hide="defaultModal" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">ยกเลิก</button>
-                                    </div>
-                                </div>
-                            </div>
-                         </div> --}}
-                        {{-- <svg class="w-10 h-10 mb-2 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5 5a3 3 0 015-2.236A3 3 0 0114.83 6H16a2 2 0 110 4h-5V9a1 1 0 10-2 0v1H4a2 2 0 110-4h1.17C5.06 5.687 5 5.35 5 5zm4 1V5a1 1 0 10-1 1h1zm3 0a1 1 0 10-1-1v1h1z" clip-rule="evenodd"></path><path d="M9 11H3v5a2 2 0 002 2h4v-7zM11 18h4a2 2 0 002-2v-5h-6v7z"></path></svg>
-                        <a href="{{ route('project.request') }}">
-                            <h5 class="mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white">นักศึกษาจองโปรเจกต์</h5>
-                        </a> --}}
-
                     </div>
 
 
@@ -216,18 +121,17 @@
 
                             <div class="w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
                                 @foreach ($projects as $project )
-                                <h5 class="mb-2 text-3xl text-left font-bold text-gray-900 dark:text-white">{{ $project->title }}</h5>
-                                <p class="mb-5 text-left text-gray-500 sm:text-lg dark:text-gray-400">{{ $project->detail }}</p>
+                                <p class="mb-2 text-2xl text-left font-bold text-gray-900 dark:text-white">{{ $project->title }}</p>
+                                <p class="mb-5 text-left text-gray-500 sm:text-lg dark:text-gray-400">รายละเอียด : {{ $project->detail }}</p>
 
-
+                                <p class="mb-5 text-left text-gray-500 sm:text-lg dark:text-gray-400">สมาชิกในกลุ่ม : </p>
                                 @foreach ($project->student_reservetion_list as $key => $member )
                                 <p class="mb-5 text-left text-gray-500 sm:text-lg dark:text-gray-400">{{ $key+1 }}.{{ $member }}</p>
 
                                 @endforeach
+                                
+                               <a href="{{ route('teacher.project.detail.teacher') }}" class="flex justify-left">เพิ่มเติม</a>
 
-
-
-                                </div>
                                 <div class="flex justify-center gap-4 mx-auto">
                                     <a href="#" class="w-1/3 sm:w-auto bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
 
@@ -248,6 +152,9 @@
                                             <div class="-mt-1 font-sans text-sm font-semibold">Comment</div>
                                         </div>
                                     </a>
+                                </div>
+
+
                                 </div>
                                 @endforeach
 
