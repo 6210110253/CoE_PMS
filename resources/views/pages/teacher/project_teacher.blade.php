5c1e00d74907a0d87b7dc345d7ce42d391c04a70
6210110566 <6210110566@psu.ac.th>
2023-02-08 19:53:45 +0700

    home admin

--- resources/views/pages/teacher/project_teacher.blade.php
+++ resources/views/pages/teacher/project_teacher.blade.php
@@ -2,7 +2,7 @@
     <div>
          <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
              <div class="container mx-auto px-6 py-8">
-                <div class="flex justify-center gap-4 mx-auto>
+                <div class="flex justify-center gap-4 >
                     <div class="w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
 
 
