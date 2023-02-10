1010f07e8079ccac805a0e8d1f5a39dc032437fa
6210110566 <6210110566@psu.ac.th>
2023-02-10 14:10:59 +0700

    update project student all

--- routes/student.php
+++ routes/student.php
@@ -10,8 +10,7 @@ Route::controller(ProjectController::class)->name('student.')->prefix('student/'
     Route::get('/project-select', 'project_select')
         ->name('project.select');
     
-    Route::get('/project-detail', 'project_detail')
-        ->name('project.detail');
+    
     Route::get('/project-propose', 'create')
         ->name('project.propose');
     Route::post('/project/store','store')
