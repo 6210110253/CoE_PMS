355d0822a228a61914da85acbf3d13c396423225
ฟาตอนียะห์ หะยีมายอ <najwa.26252@gmail.com>
2023-02-11 15:57:35 +0700

    update

--- resources/views/pages/teacher/project_detail_teacher.blade.php
+++ resources/views/pages/teacher/project_detail_teacher.blade.php
@@ -157,13 +157,10 @@
                         </div>
 
                         <button data-modal-target="authentication-modal" data-modal-toggle="authentication-modal" class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
-                            Comment
+                            Reject
                         </button>
 
-                        <div class="flex items-center justify-between">
-                            <span class="text-3xl font-bold text-gray-900 dark:text-white"></span>
-                            <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Reject</a>
-                        </div>
+
                     </div>
                  </div>
 
