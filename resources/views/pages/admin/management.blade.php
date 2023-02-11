7fab13b292b221d5308f225aba44d75f12dda461
ฟาตอนียะห์ หะยีมายอ <najwa.26252@gmail.com>
2023-02-11 16:28:54 +0700

    update

--- resources/views/pages/admin/management.blade.php
+++ resources/views/pages/admin/management.blade.php
@@ -1,24 +1,96 @@
 <x-app-layout>
     <div>
          <main class="flex-1 overflow-x-hidden overflow-y-auto bg-gray-200">
+
              <div class="container mx-auto px-6 py-8">
 
-                <div class="w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
+                 <div class="">
 
-                    <H1>Content</H1>
+                     <div class="w-full p-8  bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
 
-                </div>
+                         <p class="text-lg text-gray-900 dark:text-white">สร้างเทอม/ปีที่จะใช้งาน</p>
 
-
-
-
-
+                         <div class="relative overflow-x-auto">
+                                 <div class="flex justify-end pb-4 bg-white dark:bg-gray-900">
 
 
+                                     <label for="table-search" class="sr-only">Search</label>
+                                     <div class="relative mt-1">
+                                         <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                                             <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+                                         </div>
+                                         <input type="text" id="table-search" class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Search for items">
+                                     </div>
+
+                                 </div>
+
+                            <form class="flex flex-justify-left gap-4" method="post" action="#">
+                                <label class="">Name</label>
+                                <input type="text" id="table-search" class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="กรอกข้อมูล">
+
+                                <label class="">year</label>
+                                <input type="text" id="table-search" class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="กรอกข้อมูล">
+
+                                <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create</button>
+                            </form>
+
+                                <br>
+                             <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+                                 <thead class="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
+                                     <tr>
+                                         <th scope="col" class="px-6 py-3 rounded-l-lg">
+                                             No
+                                         </th>
+                                         <th scope="col" class="px-6 py-3">
+                                             Name
+                                         </th>
+                                         <th scope="col" class="px-6 py-3 rounded-r-lg">
+                                             Year
+                                         </th>
+                                         <th scope="col" class="px-6 py-3 rounded-r-lg">
+                                             Action
+                                         </th>
+                                     </tr>
+                                 </thead>
+                                 <tbody>
+                                     <tr class="bg-white dark:bg-gray-800">
+                                         <th scope="row" class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                             Apple MacBook Pro 17"
+                                         </th>
+                                         <td class="px-6 py-4">
+                                             1
+                                         </td>
+                                         <td class="px-6 py-4">
+                                             $2999
+                                         </td>
+                                         <td class="px-6 py-4">
+                                            Toggle
+                                         </td>
+                                     </tr>
 
 
+                                 </tbody>
+                                 <tfoot>
+                                     <tr class="font-semibold text-gray-900 dark:text-white">
+                                         <th scope="row" class="px-6 py-3 text-base">Total</th>
+                                         <td class="px-6 py-3">3</td>
+                                         <td class="px-6 py-3">21,000</td>
+                                         <td class="px-6 py-4">$2999</td>
+                                     </tr>
+                                 </tfoot>
+                             </table>
+                         </div>
+
+
+
+
+                     </div>
+
+                        <br>
+              </div>
              </div>
          </main>
      </div>
  </div>
  </x-app-layout>
+
