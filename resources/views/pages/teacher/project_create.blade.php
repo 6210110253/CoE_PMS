77233ac14b2e85049ce2fbce6e0190a00e228a2f
6210110566 <6210110566@psu.ac.th>
2023-02-15 18:24:34 +0700

    can update project teacher create by teacher in db

--- resources/views/pages/teacher/project_create.blade.php
+++ resources/views/pages/teacher/project_create.blade.php
@@ -12,8 +12,9 @@
 
                 <div class="w-full p-4  bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
 
-                    <form id="propose_project"
-                        action="{{ route('teacher.project.store') }}"
+                    <form id="project_teacher"
+                   
+                        action=" {{ Request::route()->getName() == 'teacher.project.create' ?  route('teacher.project.store') : route('teacher.project.update', $project) }}"
                         method="post"
                         enctype="multipart/form-data" >
                         @csrf
@@ -128,7 +129,7 @@
 
                     <div class="mb-6">
                         <label for="member_count" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Number Member <span class="text-red-500">*</span></label>
-                        <input type="text" id="member_count" name="member_count" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="Member" required>
+                        <input type="text" value="{{$project->member_count ?? ''}}" id="member_count" name="member_count" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="Member" required>
                     </div>
 
                     <div class="mb-6">
@@ -144,7 +145,7 @@
 
 
 
-                    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Create Project</button>
+                    <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">submit</button>
                     </form>
 
                 </div>
@@ -154,7 +155,7 @@
     </div>
 </div>
 <script>
-    $("#propose_project").validate({
+    $("#project_teacher").validate({
         rules: {
             title: {
                 required: true,
