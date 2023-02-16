704a0695518f47748d399cc9b79d7c6b6053566a
6210110566 <6210110566@psu.ac.th>
2023-02-16 19:53:21 +0700

    edit button view more

--- resources/views/pages/teacher/project_teacher.blade.php
+++ resources/views/pages/teacher/project_teacher.blade.php
@@ -171,6 +171,7 @@
                                             <p class="font-normal text-gray-700 dark:text-gray-400">Description : {{ $project->detail }}</p>
                                         </a>
 
+                                        <br>
 
                                         <div class="flex items-center justify-between">
                                             <a href="{{ route('teacher.project.detail.teacher.create', $project) }}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">View more</a>
