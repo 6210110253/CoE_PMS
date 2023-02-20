b6e3ef6420dfd179aa2fd983bcae16ddda520d13
ฟาตอนียะห์ หะยีมายอ <najwa.26252@gmail.com>
2023-02-20 22:17:34 +0700

    form create submission

--- resources/views/pages/admin/form_create_submission.blade.php
+++ resources/views/pages/admin/form_create_submission.blade.php
@@ -19,14 +19,31 @@
                             <input type="text" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="หัวข้อการส่งงาน" required>
                         </div>
                         <div class="mb-6">
+                            <label for="status" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Process <span class="text-red-500">*</span></label>
+                            <select name="status" id="status" >
+                                <option value="">--select process--</option>
+                                <option value="">Pre-Project</option>
+                                <option value="">Project I</option>
+                                <option value="">Project II</option>
 
-                            <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Round</label>
-                            <input type="text" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="Midterm/Final" required>
+                            </select>
                         </div>
                         <div class="mb-6">
+                            <label for="status" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Semester <span class="text-red-500">*</span></label>
+                            <select name="status" id="status" >
+                                <option value="">--select term/year--</option>
+                                <option value="">ปี 3 เทอม 2 2566</option>
+                                <option value="">ปี 3 เทอม 1 2566</option>
+                            </select>
+                        </div>
 
-                            <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Process</label>
-                            <input type="text" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="Pre-Project/ProjectI/ProjectII" required>
+                        <div class="mb-6">
+                            <label for="status" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Type <span class="text-red-500">*</span></label>
+                            <select name="status" id="status" >
+                                <option value="">--select type--</option>
+                                <option value="">การนัดพบอาจารย์ที่ปรึกษา</option>
+                                <option value="">ส่งรายงาน</option>
+                            </select>
                         </div>
 
 
