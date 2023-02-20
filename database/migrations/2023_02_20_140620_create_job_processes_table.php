1ae2203ddc0a637de3c2dcbbc4bc9d0ac2b48172
6210110566 <6210110566@psu.ac.th>
2023-02-20 21:56:18 +0700

    update model JopProcessed&Processed

---
+++ database/migrations/2023_02_20_140620_create_job_processes_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('job_processes', function (Blueprint $table) {
+            $table->id();
+            $table->enum("process", ['Pre-Project', 'Project I', 'Project II', 'Finished', 'Cancel'])->default('Pre-Project');
+            $table->foreignId('semester_id')
+                ->constrained()
+                ->cascadeOnDelete()
+                ->nullable();
+            $table->string("topic")
+                ->index()
+                ->nullable();
+            $table->enum("type", ['appointment', 'submission'])->default('appointment');
+            $table->dateTime("start_date")
+                ->nullable();
+            $table->dateTime("end_date")
+                ->nullable();
+            $table->enum("status", ['draft', 'publish'])->default("draft");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('job_processes');
+    }
+};
