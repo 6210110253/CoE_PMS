83423be483780076484b31ea492a406714443989
ฟาตอนียะห์ หะยีมายอ <najwa.26252@gmail.com>
2023-02-27 15:13:02 +0700

    update fe

--- resources/views/pages/student/project/project_detail.blade.php
+++ resources/views/pages/student/project/project_detail.blade.php
@@ -4,13 +4,19 @@
             <div class="container mx-auto px-6 py-8">
                 <div class="flex items-center justify-between">
                             <span class="text-3xl font-bold text-gray-900 dark:text-white"></span>
-                            <a href="{{ route('student.project.select') }}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Back</a>
+                            <a href="{{ route('student.project.select') }}">
+                                    <button class="flex items-center bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
+                                            <path fill-rule="evenodd" d="M10.707,15.707l-5-5C5.512,10.512,5,10.265,5,9.914s0.512-0.598,0.707-0.793l5-5c0.391-0.391,1.023-0.391,1.414,0s0.391,1.023,0,1.414L8.414,8H15c0.553,0,1,0.447,1,1s-0.447,1-1,1H8.414l3.707,3.707C11.098,14.684,11.195,14.854,11.195,15.025C11.195,15.195,11.098,15.367,10.707,15.707z" />
+                                        </svg>
+                                        <span>Back</span>
+                                    </button>
+                            </a>
                 </div>
+
                 <br>
 
                 <div class="w-full p-4 text-center bg-white border border-gray-200 rounded-lg shadow sm:p-8 dark:bg-gray-800 dark:border-gray-700">
-
-
 
                     <div class="relative overflow-x-auto">
                         <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
@@ -165,11 +171,14 @@
                         @if(empty(hasProjectList(Auth::id())))
 
                             @if(@$project_reservation->status == 'wait')
-                                รออาจารย์อนุมัติ
+                                <div class="w-full bg-gradient-to-tr from-orange-300 to-orange-300 py-4">
+                                    <h1 class="text-center text-orange-800 text-xl font-bold">pending approval</h1>
+                                </div>
                             @else
                                 <div class="flex justify-center gap-4 mx-auto">
-                                        <button id="reservation" class="w-1/3 sm:w-auto bg-gray-800 hover:bg-gray-700 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
-                                                <span class="-mt-1 font-sans text-sm font-semibold">Reservation</span>
+                                        <button id="reservation" class="w-1/3 sm:w-auto bg-green-500 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-gray-300 text-white rounded-lg inline-flex items-center justify-center px-4 py-2.5 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700">
+                                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30"><path fill="currentColor" d="M13 7h-2v4H7v2h4v4h2v-4h4v-2h-4V7zm-1-5C6.48 2 2 6.48 2 12s4.48 10 10 10s10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8s8 3.59 8 8s-3.59 8-8 8z"/></svg>
+                                            <span class="-mt-1 font-sans text-sm font-semibold">Reservation</span>
                                         </button>
                                 </div>
                             @endif
--- resources/views/pages/student/project/project_list.blade.php
+++ resources/views/pages/student/project/project_list.blade.php
@@ -4,8 +4,16 @@
             <div class="container mx-auto px-6 py-8">
                 <div class="flex items-center justify-between">
                             <span class="text-3xl font-bold text-gray-900 dark:text-white"></span>
-                            <a href="{{ route('student.project.select') }}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Back</a>
+                            <a href="{{ route('student.project.select') }}">
+                                    <button class="flex items-center bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded">
+                                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 mr-2" viewBox="0 0 20 20" fill="currentColor">
+                                            <path fill-rule="evenodd" d="M10.707,15.707l-5-5C5.512,10.512,5,10.265,5,9.914s0.512-0.598,0.707-0.793l5-5c0.391-0.391,1.023-0.391,1.414,0s0.391,1.023,0,1.414L8.414,8H15c0.553,0,1,0.447,1,1s-0.447,1-1,1H8.414l3.707,3.707C11.098,14.684,11.195,14.854,11.195,15.025C11.195,15.195,11.098,15.367,10.707,15.707z" />
+                                        </svg>
+                                        <span>Back</span>
+                                    </button>
+                            </a>
                 </div>
+
                 <br>
                 <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
                     <thead class="text-xs text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
@@ -23,8 +31,8 @@
                     </thead>
                     <tbody>
                         @foreach ($projects as $project )
-                            
-                        
+
+
                          <tr class="bg-white dark:bg-gray-800">
                             <td class="px-6 py-4">
                                 {{ $project->semester->name }}
@@ -34,11 +42,11 @@
                              </td>
                              <td class="px-6 py-4">
                                 <span class="text-3xl font-bold text-gray-900 dark:text-white"></span>
-                                <a href="{{ route('teacher.project.detail', $project) }}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">View more</a>
+                                <a href="{{ route('teacher.project.detail', $project) }}" class="text-white bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">View more</a>
                              </td>
                          </tr>
                         @endforeach
-                
+
 
 
         {{-- @foreach ($projects as $project )
@@ -53,7 +61,7 @@
                     <p class="font-normal text-gray-700 dark:text-gray-400">Description : {{ $project->detail }}</p>
                 </a>
 
-               
+
                 <div class="flex items-center justify-between">
                     <span class="text-3xl font-bold text-gray-900 dark:text-white"></span>
                     <a href="{{ route('teacher.project.detail', $project) }}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">View Detail</a>
