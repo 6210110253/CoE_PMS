21bb7a65e96ef8f7b523900e9f0ed536e8809815
6210110566 <6210110566@psu.ac.th>
2023-03-01 18:47:11 +0700

    update

--- resources/views/pages/teacher/project_teacher.blade.php
+++ resources/views/pages/teacher/project_teacher.blade.php
@@ -119,7 +119,7 @@
                                             @endif
                                         </a>
                                         <br>
-                                        <div class="froutelexcenter justify-between">
+                                        <div class="flexcenter justify-between">
                                             <a href="{{ route('teacher.project.detail.teacher', $project_reservation) }}" class="text-white bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">View more</a>
                                         </div>
                                     </div>
