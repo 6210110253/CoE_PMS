d7628aa373d7ec240f418ba3c2d932c6d2e29407
6210110566 <6210110566@psu.ac.th>
2023-03-14 15:28:45 +0700

    meeting edit

--- resources/views/pages/student/meeting/meeting_list.blade.php
+++ resources/views/pages/student/meeting/meeting_list.blade.php
@@ -19,10 +19,15 @@
                 <p class="font-normal text-gray-700 dark:text-gray-400">สถานะ {{ $meeting->status}}</p>
             </a>
     
-    
-            <div class="flex justify-center">
-                <span class="text-3xl font-bold text-gray-900 dark:text-white"></span>
-                <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Cancel</a>
+            <div class="flex justify-center gap-4">
+                <div>
+                    <span class="text-3xl font-bold text-gray-900 dark:text-white"></span>
+                    <a href="{{ route('student.meeting.edit' , $meeting->id) }}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Edit</a>
+                </div>
+                <div>
+                    <span class="text-3xl font-bold text-gray-900 dark:text-white"></span>
+                    <a href="#" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Cancel</a>
+                </div>
             </div>
             
         </div> 
